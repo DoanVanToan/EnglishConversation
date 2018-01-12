d71e0c2682038b821daec27f9243d96205910e27
anhpham2403 <phamtheanh243@gmail.com>
2018-01-12 09:31:06 +0700

    fix create comment

--- app/src/main/java/com/framgia/englishconversation/screen/videoDetail/VideoDetailActivity.java
+++ app/src/main/java/com/framgia/englishconversation/screen/videoDetail/VideoDetailActivity.java
@@ -1,6 +1,5 @@
 package com.framgia.englishconversation.screen.videoDetail;
 
-import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -66,18 +65,5 @@ public class VideoDetailActivity extends BaseActivity {
     protected void onResume() {
         mViewModel.onResume();
         super.onResume();
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        switch (requestCode) {
-            case Constant.RequestCode.POST_COMMENT:
-                if (resultCode != RESULT_OK) {
-                    //TODO: Handle exception later
-                    return;
-                }
-                //TODO:Handle the main story
-                break;
-        }
     }
 }
